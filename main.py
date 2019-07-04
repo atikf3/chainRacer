4ea125fd24ae00bb5d159de78dc2fa3f00a6b5cc
anagobabatunde <anagoandy@gmail.com>
2019-07-04 19:29:24 +0200

    adding backward

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import video_dir
 import motor
 import RPi.GPIO as GPIO
 
-print ('Loading camera') 
+print ('Loading camera')
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('videoplayback.mp4')
 cap.set(3, 160)
@@ -16,19 +16,19 @@ busnum = 1          # Edit busnum to 0, if you uses Raspberry Pi 1 or 0
 
 video_dir.setup(busnum=busnum)
 car_dir.setup(busnum=busnum)
-motor.setup(busnum=busnum)     # Initialize the Raspberry Pi GPIO connected to the DC motor. 
+motor.setup(busnum=busnum)     # Initialize the Raspberry Pi GPIO connected to the DC motor.
 car_dir.home()
 print ('Starting racer')
 motor.forward()
-motor.setSpeed(24)
+motor.setSpeed(35)
 while(cap.isOpened()):
     ret, frame = cap.read()
     #cv2.imshow('frame', frame)
-    crop_img = frame[60:120, 0:160]
+    crop_img = frame
     hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
     lower_or = np.array([0, 40, 40])
     upper_or = np.array([30, 255, 255])
-    mask = cv2.inRange(hsv, lower_or, upper_or) 
+    mask = cv2.inRange(hsv, lower_or, upper_or)
     #gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(mask,(5,5),0)
     ret,thresh = cv2.threshold(blur,160,175,cv2.THRESH_BINARY)
@@ -36,33 +36,34 @@ while(cap.isOpened()):
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
         M = cv2.moments(c)
- 
+
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
- 
+
         cv2.line(crop_img,(cx,0),(cx,720),(255,0,0),1)
         cv2.line(crop_img,(0,cy),(1280,cy),(255,0,0),1)
- 
+
         cv2.drawContours(crop_img, contours, -1, (0,255,0), 1)
- 
+
         if cx >= 120:
             car_dir.turn_right()
             motor.forward()
             print ("L")
- 
+
         if cx < 120 and cx > 50:
             car_dir.home()
             motor.forward()
- 
+
         if cx <= 50:
             car_dir.turn_left()
             motor.forward()
             print ("R")
 
     else:
-        motor.forward()
+	car_dir.home()
+        motor.backward()
         print ("No line")
- 
+
     #Display the resulting frame
     cv2.imshow('preview',crop_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
